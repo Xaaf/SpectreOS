02c7b623d5984620cfec47fa13593d0e75ed3aea
Xavi <x.vandalen@outlook.com>
2023-06-04 22:04:27 +0200

    Chore: Fixed comment indentation

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -2,8 +2,8 @@
 ;   boot.asm
 ;
 
-org 0x7C00              ; Use this offset
-bits 16                 ; Start in 16-bit mode (emitting 16-bit code)
+org 0x7C00                              ; Use this offset
+bits 16                                 ; Start in 16-bit mode (emitting 16-bit code)
 
 %define ENDL 0x0D, 0x0A
 
@@ -13,14 +13,14 @@ bits 16                 ; Start in 16-bit mode (emitting 16-bit code)
 jmp short start
 nop
 
-bdb_oem:                    db 'MSWIN4.1'       ; 8 Bytes
+bdb_oem:                    db 'MSWIN4.1'           ; 8 Bytes
 bdb_bytes_per_sector:       dw 512
 bdb_sectors_per_cluster:    db 1
 bdb_reserved_sectors:       dw 1
 bdb_fat_count:              db 2
 bdb_dir_entries_count:      dw 0E0h
-bdb_total_sectors:          dw 2880             ; 1.44 Megabytes from 2880 x 512
-bdb_media_descriptor_type:  db 0F0h             ; F0 is a 3.5" floppy disk
+bdb_total_sectors:          dw 2880                 ; 1.44 Megabytes from 2880 x 512
+bdb_media_descriptor_type:  db 0F0h                 ; F0 is a 3.5" floppy disk
 bdb_sectors_per_fat:        dw 9
 bdb_sectors_per_track:      dw 18
 bdb_heads:                  dw 2
@@ -39,7 +39,7 @@ ebr_system_id:              db 'FAT12   '           ; 8 bytes
 ; Bootloader
 ;
 start:
-    jmp main            ; Make sure the prgram starts at the main section
+    jmp main                            ; Make sure the prgram starts at the main section
 
 ;
 ; Print a string to the screen.
@@ -53,16 +53,16 @@ puts:
     push bx
 
 .loop:
-    lodsb               ; Load the next character in al
+    lodsb                               ; Load the next character in al
 
-    or al, al           ; Bitwise operation between source and destination,
-                        ; storing the result in destination. Verifies if the
-                        ; next character is null
+    or al, al                           ; Bitwise operation between source and destination,
+                                        ; storing the result in destination. Verifies if the
+                                        ; next character is null
 
-    jz .done            ; Jump to the destination (in this case the .done label)
-                        ; if the zero flag is set
+    jz .done                            ; Jump to the destination (in this case the .done label)
+                                        ; if the zero flag is set
 
-    mov ah, 0x0E        ; Call bios interrupt
+    mov ah, 0x0E                        ; Call bios interrupt
     mov bh, 0
     int 0x10
 
@@ -78,29 +78,29 @@ puts:
 
 main:
     ; Set up data segments
-    mov ax, 0           ; Can't write to ds/es directly
+    mov ax, 0                           ; Can't write to ds/es directly
     mov ds, ax
     mov es, ax
 
     ; Set up the stack
     mov ss, ax
-    mov sp, 0x7C00      ; Stack goes down from where we load in memory (preventing overwriting our program)
+    mov sp, 0x7C00                      ; Stack goes down from where we load in memory (preventing overwriting our program)
 
     ; Read something from the floppy
     ; BIOS should set dl to drive number
     mov [ebr_drive_number], dl
 
-    mov ax, 1           ; LBA = 1, second sector from the disk
-    mov cl, 1           ; 1 sector to read
-    mov bx, 0x7E00      ; Data should be stored after the bootloader
+    mov ax, 1                           ; LBA = 1, second sector from the disk
+    mov cl, 1                           ; 1 sector to read
+    mov bx, 0x7E00                      ; Data should be stored after the bootloader
     call disk_read
 
     ; Print 'Hello world!'
-    mov si, msg_hello   ; Set si to msg_hello
-    call puts           ; Call the method for printing a string
+    mov si, msg_hello                   ; Set si to msg_hello
+    call puts                           ; Call the method for printing a string
 
-    cli                 ; Disable interrupts, locking the CPU in "halt" state
-    hlt                 ; Stop CPU from executing
+    cli                                 ; Disable interrupts, locking the CPU in "halt" state
+    hlt                                 ; Stop CPU from executing
 
 ;
 ; Error Handlers
@@ -176,25 +176,25 @@ disk_read:
     push dx
     push di
 
-    push cx             ; Save cl to the stack
-    call lba_to_chs     ; Convert the address
-    pop ax              ; al is the number of sectors to read
+    push cx                             ; Save cl to the stack
+    call lba_to_chs                     ; Convert the address
+    pop ax                              ; al is the number of sectors to read
     
     mov ah, 02h
-    mov di, 3           ; Retry count (floppy unreliability has the documentation recommend to retry reading three times)
+    mov di, 3                           ; Retry count (floppy unreliability has the documentation recommend to retry reading three times)
 
 .retry:
-    pusha               ; Save all registers, since we don't know what the bios modifies
-    stc                 ; Set the carry flag, since some BIOS don't set it
-    int 13h             ; If the carry flag cleared, we can jump out of the loop
-    jnc .done           ; Jump if the carry is not set
+    pusha                               ; Save all registers, since we don't know what the bios modifies
+    stc                                 ; Set the carry flag, since some BIOS don't set it
+    int 13h                             ; If the carry flag cleared, we can jump out of the loop
+    jnc .done                           ; Jump if the carry is not set
     
     ; Execute when a read fails
     popa
     call disk_reset
     
     dec di
-    test di, di         ; Check if di is zero
+    test di, di                         ; Check if di is zero
     jnz .retry
 
 .fail:
@@ -231,7 +231,7 @@ disk_reset:
 msg_hello:          db 'Hello world!', ENDL, 0
 msg_read_failed:    db 'Failed to read from disk!', ENDL, 0
 
-times 510-($-$$) db 0   ; $ is the memory offset of the current line,
-                        ; $$ is the memory offset of the beginning of the current section
+times 510-($-$$) db 0                   ; $ is the memory offset of the current line,
+                                        ; $$ is the memory offset of the beginning of the current section
 
-dw 0AA55h               ; Write 2 byte value
+dw 0AA55h                               ; Write 2 byte value
