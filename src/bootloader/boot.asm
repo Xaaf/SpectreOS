533be3b4916371e2e2b1eb08682903a4f7525d15
Xavi <x.vandalen@outlook.com>
2023-06-04 17:05:49 +0200

    Added FAT12 headers

---
+++ src/bootloader/boot.asm
@@ -0,0 +1,99 @@
+org 0x7C00              ; Use this offset
+bits 16                 ; Start in 16-bit mode (emitting 16-bit code)
+
+%define ENDL 0x0D, 0x0A
+
+;
+; FAT12 Header
+;
+jmp short start
+nop
+
+bdb_oem:                    db 'MSWIN4.1'       ; 8 Bytes
+bdb_bytes_per_sector:       dw 512
+bdb_sectors_per_cluster:    db 1
+bdb_reserved_sectors:       dw 1
+bdb_fat_count:              db 2
+bdb_dir_entries_count:      dw 0E0h
+bdb_total_sectors:          dw 2880             ; 1.44 Megabytes from 2880 x 512
+bdb_media_descriptor_type:  db 0F0h             ; F0 is a 3.5" floppy disk
+bdb_sectors_per_fat:        dw 9
+bdb_sectors_per_track:      dw 18
+bdb_heads:                  dw 2
+bdb_hidden_sectors:         dd 0
+bdb_large_sector_count:     dd 0
+
+; Extended boot record
+ebr_drive_number:           db 0                    ; 0x00 floppy, 0x80 hdd, useless
+                            db 0                    ; Reserved
+ebr_signature:              db 29h
+ebr_volume_id:              db 12h, 34h, 56h, 78h   ; Serial number, value doesn't matter
+ebr_volume_label:           db 'SPECTRE OS '        ; 11 bytes
+ebr_system_id:              db 'FAT12   '           ; 8 bytes
+
+;
+; Bootloader
+;
+start:
+    jmp main            ; Make sure the prgram starts at the main section
+
+;
+; Print a string to the screen.
+; Params:
+;       ds:si points to a string
+;
+puts:
+    ; Save the registers that will be modified
+    push si
+    push ax
+    push bx
+
+.loop:
+    lodsb               ; Load the next character in al
+
+    or al, al           ; Bitwise operation between source and destination,
+                        ; storing the result in destination. Verifies if the
+                        ; next character is null
+
+    jz .done            ; Jump to the destination (in this case the .done label)
+                        ; if the zero flag is set
+
+    mov ah, 0x0E        ; Call bios interrupt
+    mov bh, 0
+    int 0x10
+
+    jmp .loop
+
+.done:
+    ; Clean up and return
+    pop bx
+    pop ax
+    pop si
+
+    ret
+
+main:
+    ; Set up data segments
+    mov ax, 0           ; Can't write to ds/es directly
+    mov ds, ax
+    mov es, ax
+
+    ; Set up the stack
+    mov ss, ax
+    mov sp, 0x7C00      ; Stack goes down from where we load in memory (preventing overwriting our program)
+
+    ; Print 'Hello world!'
+    mov si, msg_hello   ; Set si to msg_hello
+    call puts           ; Call the method for printing a string
+
+    hlt                 ; Stop CPU from executing
+
+.halt:
+    jmp .halt
+
+msg_hello: db 'Hello world!', ENDL, 0
+
+times 510-($-$$) db 0   ; $ is the memory offset of the current line,
+                        ; $$ is the memory offset of the beginning of the current section
+
+dw 0AA55h               ; Write 2 byte value
