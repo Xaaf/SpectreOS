5e2dbffd461a4c5c238ba1adbd463316587fdb6d
Xavi <x.vandalen@outlook.com>
2023-06-05 16:05:29 +0200

    Task(Bootloader): Move into C-space

--- src/bootloader/stage2/main.asm
+++ src/bootloader/stage2/main.asm
@@ -2,53 +2,28 @@
 ;   main.asm
 ;
 
-org 0x7C00                              ; Use this offset
-bits 16                                 ; Start in 16-bit mode (emitting 16-bit code)
+bits 16
+section _ENTRY class=CODE
 
-%define ENDL 0x0D, 0x0A
+extern _cstart_
+global entry
 
-main:
-    ; Print 'Hello from Stage 2!'
-    mov si, msg_hello                   ; Set si to msg_hello
-    call puts                           ; Call the method for printing a string
+entry:
+    ; Disable interrupts while setting up the stack
+    cli
 
-.halt:
-    cli                                 ; Disable interrupts, locking the CPU in "halt" state
-    hlt                                 ; Stop CPU from executing
+    mov ax, ds
+    mov ss, ax
+    mov sp, 0
+    mov bp, sp
 
-;
-; Print a string to the screen.
-; Params:
-;       ds:si points to a string
-;
-puts:
-    ; Save the registers that will be modified
-    push si
-    push ax
-    push bx
+    sti
 
-.loop:
-    lodsb                               ; Load the next character in al
+    ; Boot drive is expected in dl, send it to cstart function
+    xor dh, dh
+    push dx
+    call _cstart_
 
-    or al, al                           ; Bitwise operation between source and destination,
-                                        ; storing the result in destination. Verifies if the
-                                        ; next character is null
-
-    jz .done                            ; Jump to the destination (in this case the .done label)
-                                        ; if the zero flag is set
-
-    mov ah, 0x0E                        ; Call bios interrupt
-    mov bh, 0
-    int 0x10
-
-    jmp .loop
-
-.done:
-    ; Clean up and return
-    pop bx
-    pop ax
-    pop si
-
-    ret
-
-msg_hello: db 'Hello from Stage 2!', ENDL, 0
+    ; Halt the system if we, for any reason, return from the cstart function
+    cli
+    hlt
